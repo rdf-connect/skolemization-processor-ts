fca74832e2d1d82191e074b34d7a453b70108fdc
smessie <smessie@smessie.com>
2024-06-25 17:27:56 +0200

    feat: Implement processor

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
-# template-processor-ts
+# blank-to-named-node-identifiers-processor-ts
 
-[![Build and tests with Node.js](https://github.com/rdf-connect/template-processor-ts/actions/workflows/build-test.yml/badge.svg)](https://github.com/rdf-connect/template-processor-ts/actions/workflows/build-test.yml)
+[![Build and tests with Node.js](https://github.com/rdf-connect/blank-to-named-node-identifiers-processor-ts/actions/workflows/build-test.yml/badge.svg)](https://github.com/rdf-connect/blank-to-named-node-identifiers-processor-ts/actions/workflows/build-test.yml)
 
-This repository contains a fully functioning dummy implementation of a logging processor written in TypeScript, meant to be used as a template in order to kickstart the development of your next processor.
+This repository contains a rdf-connect processor to map entities with blank node identifiers to equivalents with named
+node identifiers.
+It will also replace the references to the blank node identifiers with the named node identifiers.
 
-## Features
-
-At the time of writing, this repositories sets up the following boilerplate and tools for you.
-
-- Placeholder implementation of a processor which accepts input from a stream, logs it to the console, and pipes it back into the outgoing stream.
-- An initial `processor.ttl` file.
-- Vitest configuration unit testing, including GitHub Actions configuration.
-- Dependabot notifications for dependency updates.
-- Convenient publishing to GitHub Packages for every new release.
-- ESLint/Prettier linting and styling, including a Husky git hook in combination with lint-staged.
-- The MIT license.
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,119 @@
 import { Stream, Writer } from "@rdfc/js-runner";
+import { Readable } from "stream";
+import rdf from "rdf-ext";
+import formatsPretty from "@rdfjs/formats/pretty.js";
+import Serializer from "@rdfjs/serializer-turtle";
+import { v4 as uuidv4 } from "uuid";
 
 /**
- * The logging function is a very simple processor which simply logs the
- * incoming stream to the console and pipes it directly into the outgoing
- * stream.
+ * rdf-connect processor to map entities with blank node identifiers to equivalents with named node identifiers.
  *
- * @param incoming The data stream which must be logged.
- * @param outgoing The data stream into which the incoming stream is written.
+ * @param incoming The data stream which must be transformed.
+ * @param outgoing The data stream into which the resulting stream is written.
+ * @param mime The MIME type of the data stream.
  */
-export function log(
+export function processor(
     incoming: Stream<string>,
     outgoing: Writer<string>,
-): () => Promise<void> {
-    /**************************************************************************
-     * This is where you set up your processor. This includes reading         *
-     * configuration files, initializing class instances, etc. You are        *
-     * guaranteed that no data will flow in the pipeline as long as your      *
-     * processor function has not returned here.                              *
-     *                                                                        *
-     * You must therefore initialize the data handlers, but you may not push  *
-     * any data into the pipeline here.                                       *
-     **************************************************************************/
+    mime = "text/turtle",
+): void {
+    console.log("BN2NN Processor started.");
+    // Initialize the data parser
+    const parser = rdf.formats.parsers.get(mime);
+    if (!parser) {
+        throw new Error(`No parser found for MIME type ${mime}.`);
+    }
 
-    incoming.on("data", (data) => {
-        outgoing
-            .push(data) // Push data into outgoing stream.
-            .then(() => console.log(data)) // Only print if successful.
-            .finally(); // Ignore any errors.
+    // Extend formatting with pretty formats.
+    rdf.formats.import(formatsPretty);
+
+    // Initialize the data serializer
+    const serializer = new Serializer();
+
+    incoming.on("data", async (data) => {
+        // Data contains quads with blank node identifiers.
+        // We transform these to named node identifiers.
+        const rawStream = Readable.from(data);
+        const quadStream = parser.import(rawStream);
+        const dataset = await rdf.dataset().import(quadStream);
+
+        // Get all blank node identifiers.
+        const blankNodes = dataset.filter(
+            (quad) => quad.subject.termType === "BlankNode",
+        );
+
+        // Map blank node identifiers to named node identifiers.
+        blankNodes.forEach((quad) => {
+            const blankNode = quad.subject;
+
+            // Create a named node identifier.
+            const namedNode = rdf.namedNode(`urn:bn2nn-id:${uuidv4()}`);
+
+            // Find all quads with the blank node as subject and replace the subject with the named node.
+            const quadsMatchingSubject = dataset.match(blankNode);
+            quadsMatchingSubject.forEach((quad) => {
+                dataset.delete(quad);
+                dataset.add(
+                    rdf.quad(
+                        namedNode,
+                        quad.predicate,
+                        quad.object,
+                        quad.graph,
+                    ),
+                );
+            });
+
+            // Find all quads with the blank node as predicate and replace the predicate with the named node.
+            const quadsMatchingPredicate = dataset.match(null, blankNode);
+            quadsMatchingPredicate.forEach((quad) => {
+                dataset.delete(quad);
+                dataset.add(
+                    rdf.quad(quad.subject, namedNode, quad.object, quad.graph),
+                );
+            });
+
+            // Find all quads with the blank node as object and replace the object with the named node.
+            const quadsMatchingObject = dataset.match(null, null, blankNode);
+            quadsMatchingObject.forEach((quad) => {
+                dataset.delete(quad);
+                dataset.add(
+                    rdf.quad(
+                        quad.subject,
+                        quad.predicate,
+                        namedNode,
+                        quad.graph,
+                    ),
+                );
+            });
+
+            // Find all quads with the blank node as graph and replace the graph with the named node.
+            const quadsMatchingGraph = dataset.match(
+                null,
+                null,
+                null,
+                blankNode,
+            );
+            quadsMatchingGraph.forEach((quad) => {
+                dataset.delete(quad);
+                dataset.add(
+                    rdf.quad(
+                        quad.subject,
+                        quad.predicate,
+                        quad.object,
+                        namedNode,
+                    ),
+                );
+            });
+        });
+
+        // Serialize the quads with named node identifiers.
+        const resultRaw = serializer.transform(dataset);
+        await outgoing.push(resultRaw);
     });
 
     // If a processor upstream terminates the channel, we propagate this change
     // onto the processors downstream.
     incoming.on("end", () => {
-        outgoing
-            .end()
-            .then(() => console.log("Incoming stream terminated."))
-            .finally();
+        outgoing.end();
     });
-
-    /**************************************************************************
-     * Any code that must be executed after the pipeline goes online must be  *
-     * embedded in the returned function. This guarantees that all channels   *
-     * are initialized and the other processors are available. A common use   *
-     * case is the source processor, which introduces data into the pipeline  *
-     * from an external source such as the file system or an HTTP API, since  *
-     * these must be certain that the downstream processors are ready and     *
-     * awaiting data.                                                         *
-     *                                                                        *
-     * Note that this entirely optional, and you may return void instead.     *
-     **************************************************************************/
-    return async () => {
-        // await outgoing.push("You're being logged. Do not resist.");
-    };
 }
